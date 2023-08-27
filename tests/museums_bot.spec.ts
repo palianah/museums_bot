31a289d141699028c8bad9572a1603180e40da43
Palian Schulz <palian.schulz@mercedes-benz.com>
2023-08-27 22:02:23 +0200

    add museumsList and tests until booking

--- tests/museums_bot.spec.ts
+++ tests/museums_bot.spec.ts
@@ -1,126 +1,81 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 const user = "palian928@gmail.com";
 const pass = "PalianMusuemsSonntag928";
 
-test.describe('Museums', () => {
-    test('Get My Ticket for Museum für Naturkunde', async ({ page }, testInfo) => {
-        await page.goto('https://shop.museumssonntag.berlin/#/tickets/time?museum_id=37');
+const museumsList = [
+  {
+    id: "37",
+    name: "Museum für Naturkunde",
+  },
+  {
+    id: "10",
+    name: "Deutsches Technikmuseum",
+  },
+  {
+    id: "48",
+    name: "Pergamonmuseum",
+  },
+  {
+    id: "16",
+    name: "Hamburger Bahnhof",
+  },
+  {
+    id: "57",
+    name: "ANOHA",
+  },
+];
 
-        await page.getByRole('link', { name: 'ANMELDEN' }).click();
-    
-        await page.click('#emailInput');
-        await page.fill('#emailInput', user);
-    
-        await page.click('#passwordInput');
-        await page.fill('#passwordInput', pass);
-    
-        await page.click('input[type="submit"]');
-    
-        await expect(page.getByRole('heading', { name: 'Zeitfenster und Tagestickets' })).toBeVisible();
+const museumToTicketSelection = async (page, museumsId: string) => {
+  await page.locator('select[name="museum"]').selectOption([museumsId]);
+  await page.click('button[translate="ticket.filters.select"]');
+  // reset
+  // await page.click('button[translate="ticket.breadcrumbs.filters.reset"]');
+  await page.locator("time-slot-selection").first().click();
+  await page.locator("select").selectOption(["number:2"]);
+  await page.click('button[translate="common.actions.cart"]');
+};
 
-        const nothing = await page.locator('div.ticket-selection td p').textContent();
-        if (nothing?.includes('noch keine Tickets')) {
-            throw new Error('noch keine Tickets');
-        } else {
-            console.log('Tickets available, continue');
-            expect(true).toBeTruthy();
-        }
+test.describe("Museums", () => {
+  museumsList.forEach((museum) => {
+    test(`Ticket for ${museum.name} - ${museum.id}`, async ({ page }) => {
+      const museumId = museum.id;
+      await page.goto(
+        `https://shop.museumssonntag.berlin/#/tickets/time?museum_id=${museumId}`
+      );
+
+      await page.getByRole("link", { name: "ANMELDEN" }).click();
+
+      await page.click("#emailInput");
+      await page.fill("#emailInput", user);
+
+      await page.click("#passwordInput");
+      await page.fill("#passwordInput", pass);
+
+      await page.click('input[type="submit"]');
+
+      await expect(
+        page.getByRole("heading", {
+          name: "Willkommen! Bitte wählen Sie ein Museum aus.",
+        })
+      ).toBeVisible();
+
+      // step add to cart
+      await museumToTicketSelection(page, museumId);
+
+      // select one more time 2 tickets
+      await page.click('button[translate="cart.detail.actions.continue"]');
+
+      // step add to cart 2
+      await museumToTicketSelection(page, museumId);
+
+      // buy ticket finally
+      await page.click('button[translate="cart.detail.actions.checkout"]');
+
+      // booked page
+      await expect(
+        page.getByRole("heading", { name: "Buchung abgeschlossen" })
+      ).toBeVisible();
     });
-
-    test('Get My Ticket for Deutsches Technikmuseum', async ({ page }, testInfo) => {
-        await page.goto('https://shop.museumssonntag.berlin/#/tickets/time?museum_id=10');
-
-        await page.getByRole('link', { name: 'ANMELDEN' }).click();
-    
-        await page.click('#emailInput');
-        await page.fill('#emailInput', user);
-    
-        await page.click('#passwordInput');
-        await page.fill('#passwordInput', pass);
-    
-        await page.click('input[type="submit"]');
-    
-        await expect(page.getByRole('heading', { name: 'Zeitfenster und Tagestickets' })).toBeVisible();
-    
-        const nothing = await page.locator('div.ticket-selection td p').textContent();
-        if (nothing?.includes('noch keine Tickets')) {
-            throw new Error('noch keine Tickets');
-        } else {
-            console.log('Tickets available, continue');
-            expect(true).toBeTruthy();
-        }
-    });
-
-    test('Get My Ticket for Pergamonmuseum', async ({ page }, testInfo) => {
-        await page.goto('https://shop.museumssonntag.berlin/#/tickets/time?museum_id=48');
-
-        await page.getByRole('link', { name: 'ANMELDEN' }).click();
-    
-        await page.click('#emailInput');
-        await page.fill('#emailInput', user);
-    
-        await page.click('#passwordInput');
-        await page.fill('#passwordInput', pass);
-    
-        await page.click('input[type="submit"]');
-    
-        await expect(page.getByRole('heading', { name: 'Zeitfenster und Tagestickets' })).toBeVisible();
-    
-        const nothing = await page.locator('div.ticket-selection td p').textContent();
-        if (nothing?.includes('noch keine Tickets')) {
-            throw new Error('noch keine Tickets');
-        } else {
-            console.log('Tickets available, continue');
-            expect(true).toBeTruthy();
-        }
-    });
-
-    test('Get My Ticket for Hamburger Bahnhof', async ({ page }, testInfo) => {
-        await page.goto('https://shop.museumssonntag.berlin/#/tickets/time?museum_id=16');
-
-        await page.getByRole('link', { name: 'ANMELDEN' }).click();
-    
-        await page.click('#emailInput');
-        await page.fill('#emailInput', user);
-    
-        await page.click('#passwordInput');
-        await page.fill('#passwordInput', pass);
-    
-        await page.click('input[type="submit"]');
-    
-        await expect(page.getByRole('heading', { name: 'Zeitfenster und Tagestickets' })).toBeVisible();
-    
-        const nothing = await page.locator('div.ticket-selection td p').textContent();
-        if (nothing?.includes('noch keine Tickets')) {
-            throw new Error('noch keine Tickets');
-        } else {
-            console.log('Tickets available, continue');
-            expect(true).toBeTruthy();
-        }
-    });
-
-    test('Get My Ticket for ANOHA', async ({ page }, testInfo) => {
-        await page.goto('https://shop.museumssonntag.berlin/#/tickets/time?museum_id=57');
-
-        await page.getByRole('link', { name: 'ANMELDEN' }).click();
-    
-        await page.click('#emailInput');
-        await page.fill('#emailInput', user);
-    
-        await page.click('#passwordInput');
-        await page.fill('#passwordInput', pass);
-    
-        await page.click('input[type="submit"]');
-    
-        await expect(page.getByRole('heading', { name: 'Zeitfenster und Tagestickets' })).toBeVisible();
-    
-        const nothing = await page.locator('div.ticket-selection td p').textContent();
-        if (nothing?.includes('noch keine Tickets')) {
-            throw new Error('noch keine Tickets');
-        } else {
-            console.log('Tickets available, continue');
-            expect(true).toBeTruthy();
-        }
-    });
+  });
 });
