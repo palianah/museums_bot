65be3a775e63d8bdb072ce98971430a1ea69489c
Palian Schulz <palian.schulz@mercedes-benz.com>
2024-02-26 15:47:07 +0100

    add anoha placeholder

---
+++ tests/anoha_bot.spec.ts
@@ -0,0 +1,6 @@
+import { test } from "@playwright/test";
+
+test.describe('ANOHA bot', () => {
+    // https://tickets.jmberlin.de/tickets/?museum_id=2
+    test.skip('TODO: write Anoha Test', () => {});
+});
